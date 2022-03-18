1f20da59b5f8ec52d29ef0ccc20d65cc0c9a6d16
saschahuberzh <74604940+saschahuberzh@users.noreply.github.com>
2022-03-18 09:44:20 +0100

    Add files via upload

---
+++ ZuulGateway/src/main/java/cts/sh/ZuulGateway/ZuulGatewayApplication.java
@@ -0,0 +1,18 @@
+package cts.sh.ZuulGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+}
---
+++ ZuulGateway/src/test/java/cts/sh/ZuulGateway/ZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package cts.sh.ZuulGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
